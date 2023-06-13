eb21278d21e5c625609f5b783e8a9558faacc522
Alona <122108963+yelloally@users.noreply.github.com>
2023-06-14 00:27:56 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,12 @@
+import argparse
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
+    parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
+    args = parser.parse_args()
+    return args.input_file, args.output_file
+
+input_file, output_file = parse_arguments()
+print(f"Plik wejściowy: {input_file}")
+print(f"Plik wyjściowy: {output_file}")
